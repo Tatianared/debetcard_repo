66c5badc2a42a568b25429d859aa15bf0eba0cc8
Краснова Татьяна <redtanya@yandex.ru>
2022-08-07 16:31:34 +0300

    add options

--- src/test/java/DebetCardTest.java
+++ src/test/java/DebetCardTest.java
@@ -1,19 +1,27 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DebetCardTest {
 
     WebDriver driver;
+
+    public void setUp(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
 
     @BeforeAll
     static void setupAll() {
@@ -39,7 +47,7 @@ class DebetCardTest {
         driver.findElement(By.className("button")).click();
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
         String actual = driver.findElement(By.cssSelector("[data-test-id=order-success")).getText().trim();
-        assertEquals(expected,actual);
+        assertEquals(expected, actual);
 
     }
 
