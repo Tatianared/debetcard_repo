81e40557c1ad1b030633fb2bbb9ce9c349745f8b
Краснова Татьяна <redtanya@yandex.ru>
2022-08-07 18:59:11 +0300

    корректировка build.gradle

--- src/test/java/ChromeTest.java
+++ src/test/java/ChromeTest.java
@@ -1,4 +1,4 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,7 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
+import io.github.bonigarcia.wdm.WebDriverManager;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ChromeTest {
